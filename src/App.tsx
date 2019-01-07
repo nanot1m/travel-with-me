11d88e5fc508252f00df4642ea2cc33b4daed662
vkochev <worack@ya.ru>
2019-01-07 15:58:39 +0500

    Dev (#1)
    * Trip and Trip Point screens
    
    * navigation between Trips and Trip Points
    
    * removed withRouter, use NavLink instead button

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,9 @@ import { Switch, Route } from "react-router-dom";
 
 import { IndexScreen } from "./screens/IndexScreen";
 import { AddTripScreen } from "./screens/AddTripScreen";
-
+import { TripScreen } from "./screens/TripScreen";
+import { AddTripPointScreen } from "./screens/AddTripPointScreen";
+import { TripPointScreen } from "./screens/TripPointScreen";
 class App extends Component {
   render() {
     return (
@@ -12,6 +14,15 @@ class App extends Component {
         <Switch>
           <Route exact path="/" component={IndexScreen} />
           <Route path="/add-trip" component={AddTripScreen} />
+          <Route exact={true} path="/trip/:tripId" component={TripScreen} />
+          <Route
+            path="/trip/:tripId/add-point"
+            component={AddTripPointScreen}
+          />
+          <Route
+            path="/trip/:tripId/point/:pointId"
+            component={TripPointScreen}
+          />
         </Switch>
       </div>
     );
---
+++ src/screens/AddTripPointScreen.tsx
@@ -0,0 +1,97 @@
+import React from "react";
+import { NavLink, RouteComponentProps } from "react-router-dom";
+import {
+  ErrorMessage,
+  Field,
+  FieldProps,
+  Formik,
+  Form,
+  FormikActions
+} from "formik";
+import uuid from "uuid";
+
+import { StoreConsumer } from "../StoreProvider";
+
+type AddTripPointScreenProps = RouteComponentProps<{
+  tripId: string;
+}>;
+
+interface AddTripPointFormValues {
+  name: string;
+}
+
+const initialValues: AddTripPointFormValues = {
+  name: ""
+};
+
+function validate(fields: AddTripPointFormValues) {
+  const errors: Partial<AddTripPointFormValues> = {};
+
+  if (fields.name.trim() === "") {
+    errors.name = "Trip Point name is required";
+  }
+
+  return errors;
+}
+
+export function AddTripPointScreen(props: AddTripPointScreenProps) {
+  const { tripId } = props.match.params;
+  return (
+    <StoreConsumer>
+      {store => {
+        const trip = store.trips.get(tripId);
+        function handleSubmit(
+          values: AddTripPointFormValues,
+          actions: FormikActions<AddTripPointFormValues>
+        ) {
+          if (!trip) return;
+          trip.addPoint(uuid.v4(), values.name);
+          actions.resetForm();
+        }
+
+        if (!trip)
+          return (
+            <div>
+              <div>
+                <NavLink to="/">Main Page</NavLink>
+              </div>
+              <h1>we don't know about this trip yet</h1>
+            </div>
+          );
+
+        return (
+          <div>
+            <div>
+              <NavLink to={`/trip/${trip.id}`}>Back to trip</NavLink>
+            </div>
+            <h1>Add Trip Point</h1>
+            <Formik<AddTripPointFormValues>
+              initialValues={initialValues}
+              validate={validate}
+              onSubmit={handleSubmit}
+            >
+              <Form>
+                <div>
+                  <Field
+                    name="name"
+                    render={({ field }: FieldProps<AddTripPointFormValues>) => (
+                      <input
+                        type="text"
+                        {...field}
+                        placeholder="Trip Point Name"
+                      />
+                    )}
+                  />
+                  <ErrorMessage name="name" />
+                </div>
+                <div>
+                  <button type="submit">Add</button>
+                </div>
+              </Form>
+            </Formik>
+          </div>
+        );
+      }}
+    </StoreConsumer>
+  );
+}
